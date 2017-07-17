23f260c80507c49a07a90a75860e22fe8de6706d
sebtn <sdebedout@gmail.com>
2017-07-17 17:15:36 -0400

    refactor hash function to es5

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 /*Set up instructions for mongoose*/
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 const Schema = mongoose.Schema 
 
 /*Model construction, fields to use*/
@@ -7,6 +8,20 @@ const userSchema = new Schema({
   email:{ type: String, unique: true, lowercase: true },
   password: String
 }) 
+
+// onSave hook encrypt
+userSchema.pre('save', function (next)  {
+  const user = this
+  bcrypt.genSalt(10, function (err, salt)  {
+    if(err) { return next(err) }
+
+    bcrypt.hash(user.password, salt, null, function (err, hash)  {
+      if(err) { return next(err) }
+      user.password = hash
+      next()
+    })
+  })
+})
 
 /*Model class for all users*/
 const modelClass = mongoose.model('user', userSchema)
