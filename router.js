2936df3ab8c3fc862a196956804f1bf0e6aa4d6a
sebtn <sdebedout@gmail.com>
2017-07-17 21:48:48 -0400

    tree file added

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const requireAuth = passport.authenticate('jwt', {session:false})
 
 module.exports =  (app) => {
   app.get('/', requireAuth, (req, res) => {
-    res.send({hi: "there"})
+    res.send( {hi: "there"} )
   })
   /*POST typeOf http-req */
   app.post('/signup', Authentication.signup)
---
+++ tree.js
@@ -0,0 +1,14 @@
+.
+├── config.js
+├── controllers
+│   └── authentication.js
+├── index.js
+├── models
+│   └── user.js
+├── package.json
+├── router.js
+├── services
+│   └── passport.js
+└── tree.js
+
+3 directories, 8 files
